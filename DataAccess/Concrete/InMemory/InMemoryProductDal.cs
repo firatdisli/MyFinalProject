27d53f7a0b079a18e1e20ab55bd1cc9d52b54cab
firatdisli <78796132+firatdisli@users.noreply.github.com>
2021-02-14 14:30:24 +0300

    Initial commit

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Bir veri tabanından alınmış gibi
+            _products = new List<Product> { 
+            new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitInStock=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitInStock=3},
+              new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitInStock=2},
+            new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitInStock=65},
+             new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=1500,UnitInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //Öncelikle Silinecek elemanın refrans numarasını bulmak gereklidir. Referansı bulunduktan sonra silinebilir
+            //LINQ - Language Integrated Query
+            //Product productToDelete = null;
+            //foreach(var p in _products)
+            //{
+            //    if (product.ProductId == p.ProductId)
+            //        productToDelete = p;
+            //}
+
+            //Foreach yerine Linq kodu ile aşağıdaki gibi yazılabilir. SingleOrDefault tek tek listeyi dolaşmaya yarar
+
+            Product   productToDelete = _products.SingleOrDefault(p=>p.ProductId==product.ProductId);
+
+            _products.Remove(productToDelete);
+
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün id sine sahip ürünü bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitInStock = product.UnitInStock;
+        }
+    }
+}
